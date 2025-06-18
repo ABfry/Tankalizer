92de86f75e51e87bd27af289b8d4ba26a466b878
TARO-gh <131171129+TARO-gh@users.noreply.github.com>
2025-06-18 20:24:15 +0900

    Revert "sqlログとcreateUserSchemaの変更"
    This reverts commit 6cb6317501233f012dc480fb1ff4d2238291cc27.

--- backend/src/schema/User/createUserSchema.ts
+++ backend/src/schema/User/createUserSchema.ts
@@ -2,25 +2,13 @@ import { z } from '@hono/zod-openapi';
 
 // リクエストの型
 export const createUserSchema = z.object({
-  name: z.string().max(20).openapi({
+  name: z.string().openapi({
     example: 'TARO-gh',
-    description: 'ユーザ名（20文字以内）',
+    description: 'ユーザ名',
   }),
-  oauth_app: z.enum(['github', 'google']).openapi({
-    example: 'github',
-    description: 'OAuthアプリの種類',
-  }),
-  connect_info: z.string().max(100).openapi({
-    example: 'taro-gh@gmail.com', 
-    description: 'メールアドレス',
-  }),
-  profile_text: z.string().max(255).optional().openapi({
-    example: '大阪で歌人やってます',
-    description: 'プロフィール文（任意）',
-  }),
-  icon_url: z.string().url().max(255).openapi({
-    example: 'https://example.com/file.jpg',
-    description: 'ユーザアイコンのURL（S3のURL）',
+  icon: z.string().openapi({
+    example: 'https://avatars.githubusercontent.com/u/131171129?v=4',
+    description: 'githubのアイコンURL',
   }),
 });
 
