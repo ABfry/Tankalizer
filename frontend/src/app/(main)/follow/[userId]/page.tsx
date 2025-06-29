4fbde13a878fab8104f81ce71cafa9a093be884b
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-06-29 20:57:49 +0900

    フォロー一覧ページファイル作成

---
+++ frontend/src/app/(main)/follow/[userId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Follow = () => {
+  return (
+    <main className='mx-auto max-w-sm pt-10 lg:max-w-lg'>
+      <div>aaa</div>
+    </main>
+  );
+};
+
+export default Follow;
