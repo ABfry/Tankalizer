bbc3bdfcf25c0dbf67f6803e92a3240828f1f035
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-07-04 11:49:11 +0900

    未ログイン時は「推しの短歌」ページを表示しない

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -3,27 +3,35 @@
 import React from 'react';
 import Timeline from '@/components/Timeline';
 import TabContainer from '@/components/tabcontainer/TabContainer';
+import { useSession } from 'next-auth/react';
 
 const LIMIT = 10; // 一度に取得する投稿数
 const MAX = 100; // タイムラインに表示できる最大投稿数
 
 const Page = () => {
+  const session = useSession();
+  const isLoggedIn = session.status === 'authenticated';
+
   return (
     <div className='relative min-h-screen'>
       {/* タイムライン */}
       <div className='mx-auto max-w-sm lg:max-w-lg'>
-        <TabContainer
-          items={[
-            {
-              title: 'みんなの短歌',
-              content: <Timeline limit={LIMIT} max={MAX} />,
-            },
-            {
-              title: '推しの短歌',
-              content: <Timeline limit={LIMIT} max={MAX} mode='following' />,
-            },
-          ]}
-        />
+        {isLoggedIn ? (
+          <TabContainer
+            items={[
+              {
+                title: 'みんなの短歌',
+                content: <Timeline limit={LIMIT} max={MAX} />,
+              },
+              {
+                title: '推しの短歌',
+                content: <Timeline limit={LIMIT} max={MAX} mode='following' />,
+              },
+            ]}
+          />
+        ) : (
+          <Timeline limit={LIMIT} max={MAX} />
+        )}
       </div>
     </div>
   );
